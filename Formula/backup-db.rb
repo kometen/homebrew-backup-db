87dd5d1cf5ab54575c4fc57dc574ba0b10c9078d
Claus Guttesen <kometen@gmail.com>
2024-12-06 09:23:51 +0100

    v0.1.1

--- Formula/backup-db.rb
+++ Formula/backup-db.rb
@@ -1,16 +1,9 @@
 class BackupDb < Formula
   desc "Read secrets from Azure Key Vault and backup a PostgreSQL database"
   homepage "https://github.com/kometen/backup-db"
-  url "https://github.com/kometen/backup-db/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "5635249ad9f8d5cdbd493fb4fdc94663cabb1fc1f0b98a6e4a5b0bd4c2095765"
+  url "https://github.com/kometen/backup-db/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "19b46fd1a90645f400ee80b09f425e5fbae35a2c5efad215ba260f94c424323f"
   license "MIT"
-
-  bottle do
-    root_url "https://github.com/kometen/homebrew-backup-db/releases/download/backup-db-0.1.0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5c6dac479ffb848818e63df2ad8ab99fc4046d5ce4242864ad6182da4fdbffb9"
-    sha256 cellar: :any_skip_relocation, ventura:       "fe104c35361ae661b40c405a6f5c5b3275a7fae0bc2d86426341429c2299c731"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a93aca31d58c14af1aa7a21a5255967296fa07633e12f733f73d9e09dc41f597"
-  end
 
   depends_on "rust" => :build
   depends_on "openssl@3.4"
